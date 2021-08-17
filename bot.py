abec45ddff2e4aac59cf8655adf720c29b10c495
Rafikova Rufina <rafikova0rr@gmail.com>
2021-08-17 18:31:20 +0700

    added basic menu and command handlers

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ bot.
 import os
 import logging
 
-from telegram import Update, ForceReply
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from service.facts import get_random_fact
@@ -31,13 +31,15 @@ logger = logging.getLogger(__name__)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
-def start(update: Update, context: CallbackContext) -> None:
+def start_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    message = fr'Привет, {user.mention_markdown_v2()}\!' + "\n\n"
+    message += r"Я небольшой бот для любителей котиков и очень рад знакомству с тобой\." + "\n\n"
+    message += r"Могу показать милых котиков, рассказать много интересных фактов и просто пообщаться с тобой, только попроси\." + "\n\n"
+    message += r"Набери ```/help```, чтобы посмотреть мои команды\."
+
+    update.message.reply_markdown_v2(message)
 
 
 def fact_command(update: Update, context: CallbackContext) -> None:
@@ -45,16 +47,38 @@ def fact_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(get_random_fact())
 
 
+def cat_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /cat is issued."""
+    update.message.reply_text("Этот функционал пока не готов")
+
+
+def cute_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /cute is issued."""
+    update.message.reply_text("Этот функционал пока не готов")
+
+
+def story_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /story is issued."""
+    update.message.reply_text("Этот функционал пока не готов")
+
+
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    # TODO: change it
-    update.message.reply_text('Help!')
+    message = r"К вашим услугам\! Вот что я умею:" + "\n\n"
+    message += r"```/fact``` \- расскажу интересный факт" + "\n"
+    message += r"```/cat``` \- отправлю картинку котика" + "\n"
+    message += r"```/cute``` \- отправлю милого котика^^" + "\n"
+    message += r"```/story``` \- расскажу историю" + "\n"
+
+    update.message.reply_markdown_v2(message)
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
+def unknown_command(update: Update, context: CallbackContext) -> None:
+    """Answer to unknown user command."""
     logging.info(update.message.text)
-    update.message.reply_text(update.message.text)
+    message = r"Ой, а такой команды я не знаю\.\.\. Попробуй ```/help```\."
+
+    update.message.reply_markdown_v2(message)
 
 
 def main() -> None:
@@ -66,12 +90,15 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("fact", fact_command))
+    dispatcher.add_handler(CommandHandler("cat", cat_command))
+    dispatcher.add_handler(CommandHandler("cute", cute_command))
+    dispatcher.add_handler(CommandHandler("story", story_command))
 
     # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, unknown_command))
 
     # Start the Bot
     updater.start_polling()
